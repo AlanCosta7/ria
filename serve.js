021105c8d97e382320148bb258dd87df42fca8a1
alanpc7 <alanpc7@gmail.com>
2021-03-28 13:28:04 -0300

    ajuste

--- serve.js
+++ serve.js
@@ -1,14 +1,14 @@
 const express = require("express")
 const cors = require("cors")
-const path = require('path')
-const htmlpath = path.resolve(__dirname, 'dist', 'pwa', 'index.html')
+//const path = require('path')
+// const htmlpath = path.resolve('dist/pwa')
 const app = express()
 app.use(cors())
 
 app.use(express.static('dist/pwa'))
 
 app.get('/*', (req, res) => {
-  res.sendFile(htmlpath)
+  res.sendFile('dist/pwa/index.html')
 })
 const port = process.env.PORT || 9000
 
