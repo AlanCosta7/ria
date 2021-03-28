c5bbf53e9860ffec1a2352d190daa8aa15e3d563
Jonathan <jonarc06@gmail.com>
2021-03-28 13:49:55 -0300

    ajustes

--- serve.js
+++ serve.js
@@ -2,10 +2,13 @@ const express = require("express")
 const cors = require("cors")
 const path = require('path')
 const htmlpath = path.resolve(__dirname, 'dist', 'pwa')
+serveStatic = require('serve-static')
+
 const app = express()
+
 app.use(cors())
 
-app.use(express.static(htmlpath))
+app.use(serveStatic(htmlpath))
 
 // app.get('/*', (req, res) => {
 //   console.log(htmlpath)
